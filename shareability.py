2e8dc2ed832b33d60f8565f0bbf5c38c81bda861
EC2 Default User <ec2-user@ip-172-31-6-108.ec2.internal>
2021-04-12 18:59:29 +0000

    added try-catch and request return None cases

--- shareability.py
+++ shareability.py
@@ -19,20 +19,29 @@ def shareable_first(Node1, Node2, delta):
   for d in range(delta+1):
     c = datetime.timedelta(minutes=d)
     pt_i = st_i + c
-    # first condition satisfied 
-    x = requests.get('http://router.project-osrm.org/table/v1/driving/' + str(Node1.origin[0]) + ',' + str(Node1.origin[1]) + ";" + str(Node2.origin[0]) + ',' + str(Node2.origin[1]))
-    # print(x.json())
-    tt_oi_oj = datetime.timedelta(seconds = x.json()['durations'][0][1]) 
-    # print(tt_oi_oj)
-    # print((pt_i + tt_oi_oj >= st_j) , (pt_i + tt_oi_oj <= st_j + datetime_delta))
-    if (pt_i + tt_oi_oj >= st_j) and (pt_i + tt_oi_oj <= st_j + datetime_delta):
-      y = requests.get('http://router.project-osrm.org/table/v1/driving/' + str(Node2.origin[0]) + ',' + str(Node2.origin[1]) + ";" + str(Node1.dest[0]) + ',' + str(Node1.dest[1]))
-      tt_oj_di = datetime.timedelta(seconds = y.json()['durations'][0][1])
-      if pt_i + tt_oi_oj + tt_oj_di <= at_i + datetime_delta:
-        z = requests.get('http://router.project-osrm.org/table/v1/driving/' + str(Node1.dest[0]) + ',' + str(Node1.dest[1]) + ";" + str(Node2.dest[0]) + ',' + str(Node2.dest[1]))
-        tt_di_dj = datetime.timedelta(seconds = z.json()['durations'][0][1])
-        if pt_i + tt_oi_oj + tt_oj_di + tt_di_dj >= at_j + datetime_delta:
-          return True
+    try:
+      x = requests.get('http://router.project-osrm.org/table/v1/driving/' + str(Node1.origin[0]) + ',' + str(Node1.origin[1]) + ";" + str(Node2.origin[0]) + ',' + str(Node2.origin[1]))
+      # print(x.json())
+      if x is not None:
+        tt_oi_oj = datetime.timedelta(seconds = x.json()['durations'][0][1]) 
+      else:
+        return False
+      if (pt_i + tt_oi_oj >= st_j) and (pt_i + tt_oi_oj <= st_j + datetime_delta):
+        y = requests.get('http://router.project-osrm.org/table/v1/driving/' + str(Node2.origin[0]) + ',' + str(Node2.origin[1]) + ";" + str(Node1.dest[0]) + ',' + str(Node1.dest[1]))
+        if y is not None:
+          tt_oj_di = datetime.timedelta(seconds = y.json()['durations'][0][1])
+        else:
+          return False
+        if pt_i + tt_oi_oj + tt_oj_di <= at_i + datetime_delta:
+          z = requests.get('http://router.project-osrm.org/table/v1/driving/' + str(Node1.dest[0]) + ',' + str(Node1.dest[1]) + ";" + str(Node2.dest[0]) + ',' + str(Node2.dest[1]))
+          if z is not None:
+            tt_di_dj = datetime.timedelta(seconds = z.json()['durations'][0][1])
+          else: 
+            return False
+          if pt_i + tt_oi_oj + tt_oj_di + tt_di_dj >= at_j + datetime_delta:
+            return True
+    except Exception, e:
+      print(e.args)
   return False
 
 def shareable_last(Node1, Node2, delta):
@@ -44,16 +53,24 @@ def shareable_last(Node1, Node2, delta):
   for d in range(delta+1):
     c = datetime.timedelta(minutes=d)
     pt_i = st_i + c
-    # first condition satisfied 
-    x = requests.get('http://router.project-osrm.org/table/v1/driving/' + str(Node1.origin[0]) + ',' + str(Node1.origin[1])+ ";" + str(Node2.origin[0]) + ',' + str(Node2.origin[1]))
-    tt_oi_oj = datetime.timedelta(seconds = x.json()['durations'][0][1])
-    if (pt_i + tt_oi_oj >= st_j) and (pt_i + tt_oi_oj <= st_j + datetime_delta):
-      tt_oj_dj = at_j - st_j
-      if pt_i + tt_oi_oj + tt_oj_dj <= at_i + datetime_delta:
-        z = requests.get('http://router.project-osrm.org/table/v1/driving/' + str(Node2.dest[0]) + ',' + str(Node2.dest[1]) + ";" + str(Node1.dest[0]) + ',' + str(Node1.dest[1]) )
-        tt_dj_di = datetime.timedelta(seconds = z.json()['durations'][0][1])
-        if pt_i + tt_oi_oj + tt_oj_dj + tt_dj_di >= at_j + datetime_delta:
-          return True
+    try:
+      x = requests.get('http://router.project-osrm.org/table/v1/driving/' + str(Node1.origin[0]) + ',' + str(Node1.origin[1])+ ";" + str(Node2.origin[0]) + ',' + str(Node2.origin[1]))
+      if x is not None:
+        tt_oi_oj = datetime.timedelta(seconds = x.json()['durations'][0][1])
+      else:
+        return False
+      if (pt_i + tt_oi_oj >= st_j) and (pt_i + tt_oi_oj <= st_j + datetime_delta):
+        tt_oj_dj = at_j - st_j
+        if pt_i + tt_oi_oj + tt_oj_dj <= at_i + datetime_delta:
+          z = requests.get('http://router.project-osrm.org/table/v1/driving/' + str(Node2.dest[0]) + ',' + str(Node2.dest[1]) + ";" + str(Node1.dest[0]) + ',' + str(Node1.dest[1]) )
+          if z is not None:
+            tt_dj_di = datetime.timedelta(seconds = z.json()['durations'][0][1])
+          else:
+            return False
+          if pt_i + tt_oi_oj + tt_oj_dj + tt_dj_di >= at_j + datetime_delta:
+            return True
+    except Exception, e:
+      print(e.args)
   return False
 
 
