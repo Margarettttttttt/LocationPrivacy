b63b7a260ae0ccdd5533af932dca444f33eb9e06
EC2 Default User <ec2-user@ip-172-31-81-194.ec2.internal>
2021-04-19 13:26:38 +0000

    commit changes from script.py

--- script.py
+++ script.py
@@ -23,11 +23,16 @@ def main():
     df_1d = df_1d.loc[54:101]
     del df
     delta = 3
+    count = 0
     for df in utils.dataloader_df(df_1d, buck_size = 10):
         last_consider = sh.last_consider_factory(df_2d, delta)
         last_series = df_1d.apply(last_consider, axis = 1)
         edges_series = utils.potential_ranges(last_series)
         output_shareable_edges = sh.output_shareable_edges_factory(df_2d, delta)
+        if count == 0:
+            count = 1
+            edges_series = edges_series[edges_series.apply(lambda x : False if (x[0]==54 and x[1]<=579) else True )]
+            print(edges_series.head())
         for series in utils.dataloader_series(edges_series, buck_size = 30):
             split_series = utils.split_edges_series(series,3)
             pool = mp.Pool(3)
@@ -39,7 +44,7 @@ def main():
                 with open('test.txt','a') as f:
                     for ele in out:
                         f.write(str(ele)+'\n')
-                    print('Update: wrote down some edges')
+                    print('Update: wrote down some edges up to ' + str(ele))
 
 if __name__ == "__main__":
     main()
